6645e2cdeb778c940cb53df1fc7a974584a6ace3
danielhyr <danhyunrha@gmail.com>
2021-06-27 22:54:25 -0400

    fixed a comment on video component, sorry about the false final commit

--- sprint-three/client/src/components/Video/Video.jsx
+++ sprint-three/client/src/components/Video/Video.jsx
@@ -6,10 +6,10 @@ import {v4 as uuid} from 'uuid'
 function Video(props) {
 
     return (
-// Used uuid here to keep refreshing video after user selects a different video
+// Used uuid here to keep refreshing video after user selects a different video initially, but opted for props.id to keep it from refreshing every time the user likes the video
         <div key = {props.id} className="video">
             <div className="video-thumbWrapper">
-                <video className="video__img" width="470" height="255"src = {props.source} poster={props.content}  controls/>
+                <video className="video__img" width="470" height="255" src = {props.source} poster={props.content}  controls/>
             </div>
         </div>
     )
